e0e969a92aa2fcfbe2e8949a0cafde39ca570b0a
Jeremy265 <jeremy.thom26@yahoo.fr>
2022-09-10 15:07:29 +0200

    First commit

---
+++ js/lang/en.js
@@ -0,0 +1,16 @@
+const en = () => {
+    return {
+        global: {
+            documentTitle: "Click The Cookie !",
+            pageTitle: "Welcome in Click The Cookie !",
+            maxPower : 'MAX POWER !',
+            instructions: "Click on the cookie to accelerate and reach maximum power !"
+        },
+        cookie: {
+            counterMessageBegin: 'You clicked',
+            counterMessageEndSingular: 'time',
+            counterMessageEndPlurial: 'times',
+            messageOnClick: "Click !",
+        }
+    }
+}
